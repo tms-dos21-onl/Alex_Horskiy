0e88e676c2037c06321dbb6c6e65fc35d41f4ad4
Horskiy <horskiy@gmail.com>
2024-07-09 18:35:21 +0200

    HW_35 completed

---
+++ HW_35/main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Create a networking
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = var.network_name
+  auto_create_subnetworks = true
+}
+
+# Creating subnets through a loop
+resource "google_compute_subnetwork" "subnetworks" {
+  count = length(var.subnets)
+
+  name          = element(var.subnets, count.index).name
+  ip_cidr_range = element(var.subnets, count.index).ip_cidr_range
+  region        = var.region
+  network       = google_compute_network.vpc_network.name
+}
+
+# Create instans though a loop
+resource "google_compute_instance" "vm_instances" {
+  count        = length(var.zones)
+  name         = "vm-instance-${count.index + 1}"
+  machine_type = var.machine_type
+  zone         = element(var.zones, count.index)
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.name
+    subnetwork = element(google_compute_subnetwork.subnetworks[*].name, count.index)
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
+  EOT
+}
---
+++ HW_35/output.tf
@@ -0,0 +1,11 @@
+output "network_name" {
+  value = google_compute_network.vpc_network.name
+}
+
+output "subnetworks" {
+  value = google_compute_subnetwork.subnetworks[*].name
+}
+
+output "instance_names" {
+  value = google_compute_instance.vm_instances[*].name
+}
