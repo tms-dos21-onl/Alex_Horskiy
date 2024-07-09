52866f3dc6f57b4951f65fb2f563c0c58ba006cd
Horskiy <horski@gmail.com>
2024-07-09 15:28:16 +0300

    Create conf GCP for terraform

---
+++ HW_22/variables.tf
@@ -0,0 +1,42 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "network_name" {
+  description = "Name of the VPC network"
+  type        = string
+  default     = "terraform-network"
+}
+
+variable "subnets" {
+  description = "List of subnets to create"
+  type = list(object({
+    name          = string
+    ip_cidr_range = string
+  }))
+}
+
+variable "zones" {
+  description = "List of zones for the instances"
+  type        = list(string)
+  default     = ["us-central1-a", "us-central1-b", "us-central1-c"]
+}
+
+variable "machine_type" {
+  description = "Machine type for the instances"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "image" {
+  description = "Disk image for the instances"
+  type        = string
+  default     = "debian-cloud/debian-10"
+}
