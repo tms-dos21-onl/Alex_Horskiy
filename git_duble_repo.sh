eb64c141ee1b1c25f4065e74c7ff38413e74357b
Horskiy <horski@gmail.com>
2024-07-12 11:24:06 +0300

    Test

--- git_duble_repo.sh
+++ git_duble_repo.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Проверка наличия аргумента для комментария коммита
+if [ -z "$1" ]; then
+    echo "Ошибка: Необходимо ввести комментарий для коммита."
+    echo "Использование: $0 \"Комментарий для коммита\""
+    exit 1
+fi
+
+commit_message=$1
+
+# Добавление всех изменений в индекс
+git add .
+
+# Создание коммита с указанным комментарием
+git commit -m "$commit_message"
 # List repo
 repos=("git@github.com:tms-dos21-onl/Alex_Horskiy.git" "git@github.com:Horskiy/Alex_Horskiy.git")
 
