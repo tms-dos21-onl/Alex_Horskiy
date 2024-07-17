ead2b342724bd73571d42f6670d730c132047ed9
Horskiy <horski@gmail.com>
2024-07-17 12:21:02 +0300

    HW_38 in underway

--- HW_38/modules/patient_portal/main.tf
+++ HW_38/modules/patient_portal/main.tf
@@ -1,9 +1,29 @@
-resource "google_compute_instance_group_manager" "patient_instance_group" {
-  #  settings for creating a virtual machine group
+resource "google_compute_instance_template" "patient_template" {
+  name = "patient-template"
+  machine_type = "e2-medium"
+  network_interface {
+    network = var.network_name
+    access_config {}
+  }
+  disks {
+    boot         = true
+    auto_delete  = true
+    source_image = "projects/debian-cloud/global/images/family/debian-11"
+  }
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y apache2
+    systemctl start apache2
+    echo "Hello from Patient Portal. I'm Dr.Dummy ;-)" > /var/www/html/index.html
+    EOF
 }
 
-resource "google_compute_instance_template" "patient_template" {
-  #  virtual machine template settings
+resource "google_compute_instance_group_manager" "patient_instance_group" {
+  name = "patient-instance-group"
+  base_instance_name = "patient-instance"
+  instance_template = google_compute_instance_template.patient_template.self_link
+  target_size = 1
 }
 
 output "instance_group_ip" {
